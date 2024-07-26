f6827ac44f9eb983c85a57b42dfe1196ab7c2b6a
Fengdaiqi <qwerty021015@163.com>
2024-07-27 00:04:08 +0800

    0.18
    quad

--- src/TheNextWeek/aabb.h
+++ src/TheNextWeek/aabb.h
@@ -9,7 +9,7 @@ public:
     interval x, y, z;
     aabb() {} // empty default constructor
     aabb(const interval &x, const interval &y, const interval &z)
-        : x(x), y(y), z(z) {}
+        : x(x), y(y), z(z) { pad_to_minimuns(); }
 
     aabb(const point3 &a, const point3 &b)
     {
@@ -17,6 +17,8 @@ public:
         x = (a[0] <= b[0]) ? interval(a[0], b[0]) : interval(b[0], a[0]);
         y = (a[1] <= b[1]) ? interval(a[1], b[1]) : interval(b[1], a[1]);
         z = (a[2] <= b[2]) ? interval(a[2], b[2]) : interval(b[2], a[2]);
+
+        pad_to_minimuns();
     }
 
     aabb(const aabb &box1, const aabb &box2)
@@ -82,6 +84,18 @@ public:
     }
 
     static const aabb empty, universe;
+
+private:
+    void pad_to_minimuns()
+    {
+        double delta = 0.0001;
+        if (x.size() < delta)
+            x = x.expand(delta);
+        if (y.size() < delta)
+            y = y.expand(delta);
+        if (z.size() < delta)
+            z = z.expand(delta);
+    }
 };
 
 const aabb aabb::empty = aabb(interval::empty, interval::empty, interval::empty);
